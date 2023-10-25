8076c2f5f0eddefde12bce3895b5f106c1827c79
merhmet <84395635+merhmet@users.noreply.github.com>
2023-10-25 22:08:39 +0200

    Add files via upload

---
+++ AnaMenu/08Kilitli/3Test/yeni/tv/turk-tv0/js/play-min.js
@@ -0,0 +1,189 @@
+﻿<!--
+var win=null;
+
+onerror = stopError;
+function stopError(){
+	return true;
+}
+
+
+function TRT1(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://trt1dvr.blutv.com/blutv_trt1_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+}
+
+function ATV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://trkvz-beta.daioncdn.net/atv/atv.m3u8?app=d1ce2d40-5256-4550-b02e-e73c185a314e","orta5",settings);
+	win.focus();
+}
+
+function FOXTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://foxtvdvr.blutv.com/blutv_foxtv_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+}
+
+function STARTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://startvdvr.blutv.com/blutv_startv_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+}
+
+function KANALD(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://kanalddvr.blutv.com/blutv_kanald_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function SHOWTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://showtvdvr.blutv.com/blutv_showtv_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TV85(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://tv85dvr.blutv.com/blutv_tv8_5_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TEVE2(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://teve2dvr.blutv.com/blutv_teve2_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function BEYAZTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://beyaztvdvr.blutv.com/blutv_beyaztv_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function KANAL7(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://kanal7dvr.blutv.com/blutv_kanal7_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function BLUTVAKSIYON(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_play1_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function BLUTVKOMEDI(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_play2_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function DMAX(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_dmax_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+
+function TLC(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://tlcdvr.blutv.com/blutv_tlc_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TRTCOCUK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_trtcocuk_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function BLUTVCOCUK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_cocuk_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TRTBELGESEL(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_trtbelgesel_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function HALKTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/webplayer/?s=https://halktv-live.ercdn.net/halktv/halktv.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TELE1(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://tele1dvr.blutv.com/blutv_tele1_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function NTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://ntvdvr.blutv.com/blutv_ntv_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function CNNTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://cnnturkdvr.blutv.com/blutv_cnnturk_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function HABERTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://haberturkdvr.blutv.com/blutv_haberturk_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TRTHABER(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://trthaberdvr.blutv.com/blutv_trthaber_dvr2/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function HABERGLOBAL(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_haberglobal_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function BLOOMBERGHT(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_bloomberght_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+function TRTSPOR(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/player/halkplayer/?s=https://daddy.yurendtv.biz.id/https://mn-nl.mncdn.com/blutv_trtspor_dvr/live_360p500000kbps/index.m3u8","orta5",settings);
+	win.focus();
+
+}
+
+// -->
---
+++ AnaMenu/08Kilitli/3Test/yeni/tv/turk-tv8/play/js/ch-min.js
@@ -0,0 +1,50 @@
+﻿<!--
+var win=null;
+
+onerror = stopError;
+function stopError(){
+	return true;
+}
+
+function T1(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/turk-tv8/ply/tvturk.htm?id=https://www.hdtvler.tv/show-tv-canli-hd-yayin-izle-4/","orta7",settings);
+	win.focus();
+
+}
+
+function T2(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/spor/spor/tv.html?id=http://www.ecanlitvizle.live/tv.php","orta7",settings);
+	win.focus();
+
+}
+
+function T3(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/turk-tv8/ply/tvcenter.htm?id=https://canlitv.center/canlitv","orta7",settings);
+	win.focus();
+
+}
+
+function T4(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/turk-tv8/ply/tvlive.htm?id=https://canlitv.com/trt-1","orta7",settings);
+	win.focus();
+
+}
+function T5(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/turk-tv8/ply/tvocanli.htm?id=https://www.ocanlitv.live/atv-canli-izle-4","orta7",settings);
+	win.focus();
+
+}
+
+function T6(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://saraydorf.de/tv/··/tv1/tv-kanal-list.htm","orta7",settings);
+	win.focus();
+
+}
+
+// -->
