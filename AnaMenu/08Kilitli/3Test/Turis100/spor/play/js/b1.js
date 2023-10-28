61d590ce769093684e0f148a110571f1839e8ddd
merhmet <84395635+merhmet@users.noreply.github.com>
2023-10-28 06:55:44 +0200

    Delete AnaMenu/08Kilitli/3Test/Turis100 directory

--- AnaMenu/08Kilitli/3Test/Turis100/spor/play/js/b1.js
+++
@@ -1,4 +0,0 @@
-﻿      	var url_string = window.location.href;
-var url = new URL(url_string);
-var cid = url.searchParams.get("id"); 
-document.write(" <iframe  id='frame' name='frame' src='//saraydorf.de/spor/player/webplayer/?s=https://v5.hermosabaechyachtclub.com/"+cid+"/index.m3u8' width='100%'  height='100%' frameborder='0' allowfullscreen scrolling='no'></iframe>");
--- AnaMenu/08Kilitli/3Test/Turis100/spor/play/js/vole.js
+++
@@ -1,7 +0,0 @@
-﻿      	var url_string = window.location.href;
-var url = new URL(url_string);
-var cid = url.searchParams.get("id"); 
-document.write(" <iframe  id='frame' name='frame' src='//saraydorf.de/spor/player/webplayer/?s=https://volestream.calmakardesim.site/hls/"+cid+".m3u8' width='100%'  height='100%' frameborder='0' allowfullscreen scrolling='no' style='position:absolute;' sandbox='none-popups allow-scripts allow-same-origin'></iframe>");
-
-
-
